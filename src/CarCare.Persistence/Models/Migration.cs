8de0403aeb7a34d3af6a9318719090ae4e15fae2
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-09-25 17:43:53 +0200

    Migration model [Persistence]

---
+++ src/CarCare.Persistence/Models/Migration.cs
@@ -0,0 +1,28 @@
+﻿namespace CarCare.Persistence.Models;
+
+/// <summary>
+/// A database migration model.
+/// </summary>
+public class Migration
+{
+    /// <summary>
+    /// The id of the migration.
+    /// </summary>
+    string Id { get; set; }
+
+    /// <summary>
+    /// The version of the application that this migration is bound to.
+    /// </summary>
+    string ProductVersion { get; set; }
+
+    /// <summary>
+    /// Creates a new <see cref="Migration"/> object.
+    /// </summary>
+    /// <param name="id">The id of the migration.</param>
+    /// <param name="productVersion">The product version that the migration is bound to.</param>
+    public Migration(string id, string productVersion)
+    {
+        Id = id;
+        ProductVersion = productVersion;
+    }
+}
