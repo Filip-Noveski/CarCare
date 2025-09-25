364965b2059219e8a4c4e81bca78a658a3f37d84
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-09-25 17:44:40 +0200

    IServiceCollectionExtensions [Persistence]

---
+++ src/CarCare.Persistence/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using CarCare.Persistence.Interfaces;
+using CarCare.Persistence.Services;
+using CarCare.Persistence.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarCare.Persistence.Extensions;
+
+/// <summary>
+/// Provides extensions to the <see cref="IServiceCollection"/> interface.
+/// </summary>
+public static class IServiceCollectionExtensions
+{
+    /// <summary>
+    /// Registers all Persistence level services.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+    public static void AddPersistenceServices(this IServiceCollection services)
+    {
+        services.AddSingleton<DBContext>();
+        services.AddTransient<IMigrationsHistoryRepository, MigrationsHistoryRepository>();
+    }
+}
