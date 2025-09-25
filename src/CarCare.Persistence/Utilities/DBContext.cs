476fb073216b095c3ae56485e2ac5deff9208dad
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-09-25 17:43:37 +0200

    DBContext [Persistence]

---
+++ src/CarCare.Persistence/Utilities/DBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CarCare.Persistence.Utilities;
+
+internal class DBContext
+{
+    public string ConnectionString { get; private set; }
+
+    public DBContext(IConfiguration configuration)
+    {
+        string appName = configuration["Application:Name"]!;
+        string dbName = configuration["Database:Name"]!;
+        ConnectionString = CreateConnectionString(appName, dbName);
+    }
+
+    protected static string CreateConnectionString(string appName, string dbName)
+    {
+        Environment.SpecialFolder folderType = Environment.SpecialFolder.ApplicationData;
+        string folderPath = Environment.GetFolderPath(folderType);
+        string dbPath = Path.Combine(folderPath, appName, dbName);
+        return $"Data Source={dbPath}";
+    }
+}
