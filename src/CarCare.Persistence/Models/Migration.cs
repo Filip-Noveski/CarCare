ead8dcaee685963c0e698c9b56f446f6531215ef
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-09-26 13:00:42 +0200

    Change Migration to only have int Id [Persistence]

--- src/CarCare.Persistence/Models/Migration.cs
+++ src/CarCare.Persistence/Models/Migration.cs
@@ -8,21 +8,14 @@ public class Migration
     /// <summary>
     /// The id of the migration.
     /// </summary>
-    public string Id { get; set; }
-
-    /// <summary>
-    /// The version of the application that this migration is bound to.
-    /// </summary>
-    public string ProductVersion { get; set; }
+    public int Id { get; set; }
 
     /// <summary>
     /// Creates a new <see cref="Migration"/> object.
     /// </summary>
     /// <param name="id">The id of the migration.</param>
-    /// <param name="productVersion">The product version that the migration is bound to.</param>
-    public Migration(string id, string productVersion)
+    public Migration(int id)
     {
         Id = id;
-        ProductVersion = productVersion;
     }
 }
