633aec7be5518962eff073d35a8ed3f02e348bb3
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-09-26 20:51:38 +0200

    SplashScreenContext [Processing]

---
+++ src/CarCare.Processing/Contexts/SplashScreenContext.cs
@@ -0,0 +1,35 @@
+﻿using CarCare.Processing.Abstract;
+using CarCare.Processing.Interfaces.Context;
+using CarCare.Processing.Interfaces.Service;
+
+namespace CarCare.Processing.Contexts;
+
+internal class SplashScreenContext : Context, ISplashScreenContext
+{
+    private readonly IDatabaseManagementService _dbManagementService;
+
+    public string OperationText 
+    {
+        get
+        {
+            return field;
+        }
+        private set
+        {
+            field = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public SplashScreenContext(IDatabaseManagementService dbManagementService)
+    {
+        _dbManagementService = dbManagementService;
+        OperationText = string.Empty;
+    }
+
+    public async Task InitialiseDatabaseAsync()
+    {
+        OperationText = "Verifying database existence and version...";
+        await _dbManagementService.VerifyDatabaseStateAsync();
+    }
+}
---
+++ src/CarCare.Processing/Interfaces/Context/ISplashScreenContext.cs
@@ -0,0 +1,18 @@
+﻿namespace CarCare.Processing.Interfaces.Context;
+
+/// <summary>
+/// Data context for the splash screen of the application.
+/// </summary>
+public interface ISplashScreenContext
+{
+    /// <summary>
+    /// The text that is displayed to inform the user of the pending operation.
+    /// </summary>
+    string OperationText { get; }
+
+    /// <summary>
+    /// Verifies the database existence and validity, and creates/updates it if necessary.
+    /// </summary>
+    /// <returns>An awaitable <see cref="Task"/>.</returns>
+    Task InitialiseDatabaseAsync();
+}
