b3227e05ec0c0586da912d339df65d89cef56393
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-09-26 20:50:50 +0200

    Context [Processing]

---
+++ src/CarCare.Processing/Abstract/Context.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CarCare.Processing.Abstract;
+
+/// <summary>
+/// Base class for Data Contexts of views.
+/// </summary>
+public abstract class Context : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /// <summary>
+    /// Notifies that the property with the provided name has changed.
+    /// </summary>
+    /// <param name="propertyName">The name of the changed property.</param>
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
