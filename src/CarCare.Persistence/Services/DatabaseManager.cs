d8a58d774c9f0be37b2c381e715c6ff3b7e64538
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-09-26 13:02:49 +0200

    IDatabaseManager interface and implementation [Persistence]

---
+++ src/CarCare.Persistence/Services/DatabaseManager.cs
@@ -0,0 +1,27 @@
+﻿using CarCare.Persistence.Configuration;
+using CarCare.Persistence.Interfaces;
+
+namespace CarCare.Persistence.Services;
+
+internal class DatabaseManager : IDatabaseManager
+{
+    private readonly IMigrationsHistoryRepository _historyRepository;
+    private readonly MigrationsManager _migrationsManager;
+
+    public DatabaseManager(IMigrationsHistoryRepository historyRepository, MigrationsManager migrationsManager)
+    {
+        _historyRepository = historyRepository;
+        _migrationsManager = migrationsManager;
+    }
+
+    public async Task VerifyDatabaseStateAsync()
+    {
+        if (!await _historyRepository.ExistsAsync())
+        {
+            await _migrationsManager.CreateMigrationHistoryTableAsync();
+        }
+
+        await _migrationsManager.UpdateToLatestAsync();
+        throw new NotImplementedException();
+    }
+}
