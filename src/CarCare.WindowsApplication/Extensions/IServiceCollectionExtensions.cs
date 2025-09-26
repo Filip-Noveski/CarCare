059c2042101f8bb66c3d54c6688c656e74d4a120
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-09-26 14:14:12 +0200

    MigrationsManagerTests [Persistence.Tests]

---
+++ src/CarCare.WindowsApplication/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CarCare.WindowsApplication.Extensions;
+
+/// <summary>
+/// Provides extensions to the <see cref="IServiceCollection"/> interface.
+/// </summary>
+public static class IServiceCollectionExtensions
+{
+    /// <summary>
+    /// Registers all WindowsApplication level services.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+    public static void AddWindowsApplicationServices(this IServiceCollection services)
+    {
+        services.AddSingleton<MainWindow>();
+    }
+}
---
+++ tests/CarCare.Persistence.Tests/Configuration/MigrationsManagerTests.cs
@@ -0,0 +1,48 @@
+﻿using CarCare.Persistence.Configuration;
+using CarCare.Persistence.Interfaces;
+using CarCare.Persistence.Services;
+using CarCare.Persistence.Tests.Base;
+using Dapper;
+using FluentAssertions;
+using Microsoft.Data.Sqlite;
+using Xunit.Abstractions;
+
+namespace CarCare.Persistence.Tests.Configuration;
+
+public class MigrationsManagerTests : DBTestsGroup
+{
+    private readonly DBContext _context;
+    private readonly IMigrationsHistoryRepository _historyRepository;
+    private readonly MigrationsManager _sut;
+
+    public MigrationsManagerTests(ITestOutputHelper output) : base(output)
+    {
+        _context = new(Configuration);
+        _historyRepository = new MigrationsHistoryRepository(_context);
+        _sut = new(Configuration, _context, _historyRepository);
+    }
+
+    [Fact]
+    public async Task ShouldCreateMigrationsTableAndRegisterFirstMigration()
+    {
+        // Arrange
+        
+        // Act
+        await _sut.CreateMigrationHistoryTableAsync();
+
+        // Assert
+        using SqliteConnection connection = await _context.CreateConnection();
+
+        string sql = $"""
+            SELECT COUNT(*)
+            FROM sqlite_master
+            WHERE type = 'table' AND name = '__MigrationHistory'
+            """;
+
+        int count = await connection.ExecuteScalarAsync<int>(sql);
+
+        await connection.CloseAsync();
+
+        count.Should().Be(1);
+    }
+}
