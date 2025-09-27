a3857a4fa0d7d81834fd27ed15bcbf4aa41bc5b7
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-09-27 16:30:30 +0200

    Users table migration [Persistence]

---
+++ src/CarCare.Persistence/Migrations/_migration.20250927.sql
@@ -0,0 +1,9 @@
+﻿-- Migration date: 27.09.2025
+-- Add Users table
+
+CREATE TABLE IF NOT EXISTS Users (
+    Id TEXT PRIMARY KEY,
+    Username TEXT NOT NULL UNIQUE,
+    Password TEXT NOT NULL,
+    Avatar BLOB
+);
