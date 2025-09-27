4b48b8dde83c167b9d4202144127e08eff27fe98
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-09-27 14:05:17 +0200

    DI and appsettings.json support, startup and exit logic [WindowsApp]

--- src/CarCare.WindowsApplication/App.xaml.cs
+++ src/CarCare.WindowsApplication/App.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System.Windows;
+﻿using CarCare.Persistence.Extensions;
+using CarCare.Processing.Extensions;
+using CarCare.UserInterface.Extensions;
+using CarCare.UserInterface.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
 
 namespace CarCare.WindowsApplication;
 
@@ -7,4 +14,49 @@ namespace CarCare.WindowsApplication;
 /// </summary>
 public partial class App : Application
 {
+    public static IHost AppHost { get; private set; } = null!;
+
+    public App()
+    {
+        IHostBuilder builder = Host.CreateDefaultBuilder();
+        builder.ConfigureAppConfiguration((context, config) =>
+        {
+            config.AddJsonFile("appsettings.json");
+#if DEBUG
+            config.AddJsonFile("appsettings.Development.json");
+#elif RELEASE
+            config.AddJsonFile("appsettings.Production.json");
+#endif
+        });
+        builder.ConfigureServices((context, services) =>
+        {
+            services.AddPersistenceServices();
+            services.AddProcessingServices();
+            services.AddUserInterfaceServices();
+        });
+        AppHost = builder.Build();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await AppHost.StartAsync();
+
+        Splash splash = AppHost.Services.GetRequiredService<Splash>();
+        splash.Show();
+        await splash.RunApplicationPreparationAsync();
+
+        Dashboard dashboard = AppHost.Services.GetRequiredService<Dashboard>();
+        splash.Close();
+        dashboard.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await AppHost.StopAsync();
+        AppHost.Dispose();
+
+        base.OnExit(e);
+    }
 }
