7eb3a7dda87e79c4aedbd3010a932205c55c61ad
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-09-28 17:24:53 +0200

    UserService interface and implementation [Processing]

---
+++ src/CarCare.Processing/Interfaces/Service/IUserService.cs
@@ -0,0 +1,69 @@
+﻿using CarCare.Processing.Models;
+using OneOf;
+
+namespace CarCare.Processing.Interfaces.Service;
+
+/// <summary>
+/// A service for managing <see cref="User"/> objects.
+/// </summary>
+public interface IUserService
+{
+    /// <summary>
+    /// Creates a new <see cref="User"/>.
+    /// </summary>
+    /// <param name="user">The <see cref="User"/> to create.</param>
+    /// <returns>An awaitable <see cref="Task"/>.</returns>
+    Task RegisterAsync(User user);
+
+    /// <summary>
+    /// Deletes the specified <see cref="User"/>.
+    /// </summary>
+    /// <param name="id">The id to delete by.</param>
+    /// <returns>An awaitable <see cref="Task"/>.</returns>
+    Task DeleteAsync(Guid id);
+
+    /// <summary>
+    /// Deletes the specified <see cref="User"/>.
+    /// </summary>
+    /// <param name="username">The username to delete by.</param>
+    /// <returns>An awaitable <see cref="Task"/>.</returns>
+    Task DeleteAsync(string username);
+
+    /// <summary>
+    /// Updates the <see cref="User"/> to contain the new values.
+    /// </summary>
+    /// <param name="user">The <see cref="User"/> with new parameters.</param>
+    /// <returns>An awaitable <see cref="Task"/>.</returns>
+    Task UpdateAsync(User user);
+
+    /// <summary>
+    /// Tries to log the user in based on the credentials.
+    /// </summary>
+    /// <param name="username">The username.</param>
+    /// <param name="password">The plaintext password.</param>
+    /// <returns>
+    /// The logged in <see cref="User"/> - if login was successful;
+    /// A <see cref="LoginError"/> - if login failed.
+    /// </returns>
+    Task<OneOf<User, LoginError>> LoginAsync(string username, string password);
+
+    /// <summary>
+    /// Returns the specified <see cref="User"/>.
+    /// </summary>
+    /// <param name="id">The id of the user.</param>
+    /// <returns>The discovered <see cref="User"/>.</returns>
+    Task<User> GetUserAsync(Guid id);
+
+    /// <summary>
+    /// Returns the specified <see cref="User"/>.
+    /// </summary>
+    /// <param name="username">The username of the user.</param>
+    /// <returns>The discovered <see cref="User"/>.</returns>
+    Task<User> GetUserAsync(string username);
+
+    /// <summary>
+    /// Returns all users in the database.
+    /// </summary>
+    /// <returns>An <see cref="IEnumerable{T}"/> of <see cref="User"/>.</returns>
+    Task<IEnumerable<User>> GetUsersAsync();
+}
---
+++ src/CarCare.Processing/Services/UserService.cs
@@ -0,0 +1,74 @@
+﻿using CarCare.Persistence.Interfaces;
+using CarCare.Persistence.Models;
+using CarCare.Processing.Interfaces.Service;
+using CarCare.Processing.Models;
+using Microsoft.AspNetCore.Identity;
+using OneOf;
+using System.Diagnostics;
+
+namespace CarCare.Processing.Services;
+
+internal class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IPasswordHasher<User> _hasher;
+
+    public UserService(IUserRepository userRepository, IPasswordHasher<User> hasher)
+    {
+        _userRepository = userRepository;
+        _hasher = hasher;
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        await _userRepository.DeleteAsync(id);
+    }
+
+    public async Task DeleteAsync(string username)
+    {
+        await _userRepository.DeleteAsync(username);
+    }
+
+    public async Task<User> GetUserAsync(Guid id)
+    {
+        UserDao user = await _userRepository.GetUserAsync(id);
+        return new(user);
+    }
+
+    public async Task<User> GetUserAsync(string username)
+    {
+        UserDao user = await _userRepository.GetUserAsync(username);
+        return new(user);
+    }
+
+    public async Task<IEnumerable<User>> GetUsersAsync()
+    {
+        IEnumerable<UserDao> users = await _userRepository.GetUsersAsync();
+        return users.Select(x => new User(x));
+    }
+
+    public async Task<OneOf<User, LoginError>> LoginAsync(string username, string password)
+    {
+        UserDao user = await _userRepository.GetUserAsync(username);
+        PasswordVerificationResult result = _hasher.VerifyHashedPassword(null!, user.Password, password);
+        return result switch
+        {
+            PasswordVerificationResult.Failed => new LoginError("Incorrect password"),
+            PasswordVerificationResult.Success => new User(user),
+            PasswordVerificationResult.SuccessRehashNeeded => throw new Exception("Needs handling"),
+            _ => throw new UnreachableException()
+        };
+    }
+
+    public async Task RegisterAsync(User user)
+    {
+        UserDao dao = user.ToDao();
+        await _userRepository.AddAsync(dao);
+    }
+
+    public async Task UpdateAsync(User user)
+    {
+        UserDao dao = user.ToDao();
+        await _userRepository.UpdateAsync(dao);
+    }
+}
