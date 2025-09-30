337d648c68bab1f5fe613490f6ace5599b1ba877
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-09-30 13:49:14 +0200

    Adapt to User changes in UserTests [Processing.Tests]

--- tests/CarCare.Processing.Tests/Models/UserTests.cs
+++ tests/CarCare.Processing.Tests/Models/UserTests.cs
@@ -17,17 +17,15 @@ public class UserTests
     public void ShouldCreateUserWithHashedPasswordAndNoAvatar()
     {
         // Arrange
-        Guid id = Guid.NewGuid();
         string username = "Some username";
         string password = "Some password";
 
         // Act
-        User user = User.Create(id, username, password, _hasher);
+        User user = User.Create(username, password, _hasher);
 
         // Assert
         PasswordVerificationResult passVerification = _hasher.VerifyHashedPassword(null!, user.Password, password);
 
-        user.Id.Should().Be(id);
         user.Username.Should().Be(username);
         user.Avatar.Should().BeNull();
         passVerification.Should().Be(PasswordVerificationResult.Success);
@@ -37,18 +35,16 @@ public class UserTests
     public void ShouldCreateUserWithHashedPasswordAndAvatar()
     {
         // Arrange
-        Guid id = Guid.NewGuid();
         string username = "Some username";
         string password = "Some password";
         byte[] avatar = new byte[] { 0x02, 0x13, 0x65 };
 
         // Act
-        User user = User.Create(id, username, password, avatar, _hasher);
+        User user = User.Create(username, password, avatar, _hasher);
 
         // Assert
         PasswordVerificationResult passVerification = _hasher.VerifyHashedPassword(null!, user.Password, password);
 
-        user.Id.Should().Be(id);
         user.Username.Should().Be(username);
         user.Avatar.Should().BeEquivalentTo(avatar);
         passVerification.Should().Be(PasswordVerificationResult.Success);
