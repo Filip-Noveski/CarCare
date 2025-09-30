631641497cf3a625c56f0279aecfd98cbd154e6b
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-09-30 14:17:27 +0200

    IRegisterContext [Processing]

---
+++ src/CarCare.Processing/Interfaces/Context/IRegisterContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Input;
+
+namespace CarCare.Processing.Interfaces.Context;
+
+/// <summary>
+/// A data context for the Register control.
+/// </summary>
+public interface IRegisterContext
+{
+    /// <summary>
+    /// The command to register a new user.
+    /// </summary>
+    ICommand RegisterCommand { get; }
+
+    /// <summary>
+    /// The username of the user.
+    /// </summary>
+    string Username { get; }
+
+    /// <summary>
+    /// An error that may have occurred during registration.
+    /// </summary>
+    string Error { get; }
+}
