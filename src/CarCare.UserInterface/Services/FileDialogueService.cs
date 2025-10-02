4e90e7fc82aadde5b7e0507af678beff26625dd4
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-02 17:37:03 +0200

    FileDialogueService [UserInterface]

---
+++ src/CarCare.UserInterface/Services/FileDialogueService.cs
@@ -0,0 +1,23 @@
+﻿using CarCare.Processing.Interfaces.Service;
+using Microsoft.Win32;
+using System.Windows.Media.Imaging;
+
+namespace CarCare.UserInterface.Services;
+
+internal class FileDialogueService : IFileDialogueService
+{
+    public BitmapImage? GetImageFile()
+    {
+        OpenFileDialog dialogue = new()
+        {
+            Filter = "Image Files (*.png;*jpg;*.jpeg;*.bmp)|*.png;*jpg;*.jpeg;*.bmp"
+        };
+
+        if (dialogue.ShowDialog() is true)
+        {
+            return new(new Uri(dialogue.FileName));
+        }
+
+        return null;
+    }
+}
