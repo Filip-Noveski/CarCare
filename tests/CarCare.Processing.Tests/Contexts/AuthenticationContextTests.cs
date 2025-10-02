a525423522bddf68e8ac46fe1a092b2c95a5c6a5
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-02 17:31:23 +0200

    AuthenticationContextTests [Processing.Tests]

---
+++ tests/CarCare.Processing.Tests/Contexts/AuthenticationContextTests.cs
@@ -0,0 +1,50 @@
+﻿using CarCare.Processing.Contexts;
+using FluentAssertions;
+
+namespace CarCare.Processing.Tests.Contexts;
+
+public class AuthenticationContextTests
+{
+    private readonly AuthenticationContext _sut;
+
+    public AuthenticationContextTests()
+    {
+        _sut = new();
+    }
+
+    [Fact]
+    public void ShouldRaiseRegisterRequestEvent()
+    {
+        // Arrange
+        bool called = false;
+        EventHandler a = (s, e) =>
+        {
+            called = true;
+        };
+        _sut.RegisterRequested += a;
+
+        // Act
+        _sut.OnRegisterRequested();
+
+        // Assert
+        called.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ShouldRaiseLoginRequestEvent()
+    {
+        // Arrange
+        bool called = false;
+        EventHandler a = (s, e) =>
+        {
+            called = true;
+        };
+        _sut.LoginRequested += a;
+
+        // Act
+        _sut.OnLoginRequested();
+
+        // Assert
+        called.Should().BeTrue();
+    }
+}
