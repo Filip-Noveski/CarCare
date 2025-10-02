6e64484285e9dfc94aee5deaa1f078a9c5e07837
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-02 17:32:21 +0200

    Additional commands and properties in LoginContext [Processing]

--- src/CarCare.Processing/Contexts/LoginContext.cs
+++ src/CarCare.Processing/Contexts/LoginContext.cs
@@ -4,7 +4,9 @@ using CarCare.Processing.Interfaces.Context;
 using CarCare.Processing.Interfaces.Service;
 using CarCare.Processing.Models;
 using OneOf;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace CarCare.Processing.Contexts;
 
@@ -12,10 +14,33 @@ internal class LoginContext : Context, ILoginContext
 {
     private readonly IUserService _userService;
     private readonly INavigationService _navigationService;
+    private readonly IBitmapCreatorService _bitmapService;
+    private readonly IAuthenticationContext _authenticationContext;
+
+    public event EventHandler UsernameChanged = null!;
 
     public ICommand LoginCommand { get; }
 
+    public ICommand RegisterCommand { get; }
+
+    public ICommand SelectUserCommand { get; }
+
+    public ICommand ReturnToUsersListCommand { get; }
+
+    public ObservableCollection<UserDto> Users { get; private set; }
+
     public string Username
+    {
+        get => field;
+        set
+        {
+            field = value;
+            OnPropertyChanged();
+            OnUsernameChanged();
+        }
+    }
+
+    public BitmapImage? Avatar
     {
         get => field;
         set
@@ -35,13 +60,40 @@ internal class LoginContext : Context, ILoginContext
         }
     }
 
-    public LoginContext(IUserService userService, INavigationService navigationService)
+    public LoginContext(
+        IUserService userService,
+        INavigationService navigationService,
+        IBitmapCreatorService bitmapService,
+        IAuthenticationContext authenticationContext)
     {
         _userService = userService;
         _navigationService = navigationService;
+        _bitmapService = bitmapService;
+        _authenticationContext = authenticationContext;
+        Users = new();
         Username = string.Empty;
+        Avatar = null;
         Error = string.Empty;
         LoginCommand = new Command(Login);
+        RegisterCommand = new Command(NavigateToRegister);
+        SelectUserCommand = new Command(SelectUser);
+        ReturnToUsersListCommand = new Command(ReturnToUsersList);
+    }
+
+    public async Task InitialiseAsync()
+    {
+        IEnumerable<UserDto> users = (await _userService.GetUsersAsync())
+            .Select(x => new UserDto(x.Id, x.Username, x.Avatar));
+        Users.Clear();
+        foreach (UserDto user in users)
+        {
+            Users.Add(user);
+        }
+
+        if (Users.Count is 0)
+        {
+            _authenticationContext.OnRegisterRequested();
+        }
     }
 
     private async void Login(object? parameter)
@@ -57,5 +109,34 @@ internal class LoginContext : Context, ILoginContext
         result.Switch(
             user => _navigationService.NavigateTo<IDashboardContext>(args.Window),
             error => Error = error.Message);
+    }
+
+    private void NavigateToRegister(object? parameter)
+    {
+        _authenticationContext.OnRegisterRequested();
+    }
+
+    private void SelectUser(object? parameter)
+    {
+        if (parameter is not string username)
+        {
+            Error = "An internal error occurred.";
+            return;
+        }
+
+        UserDto user = Users.First(x => x.Username == username);
+        Username = username;
+        Avatar = user.Avatar;
+    }
+
+    private void ReturnToUsersList(object? parameter)
+    {
+        Username = string.Empty;
+        Avatar = null;
+    }
+
+    private void OnUsernameChanged()
+    {
+        UsernameChanged?.Invoke(this, EventArgs.Empty);
     }
 }
--- src/CarCare.Processing/Interfaces/Context/ILoginContext.cs
+++ src/CarCare.Processing/Interfaces/Context/ILoginContext.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Input;
+﻿using CarCare.Processing.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace CarCare.Processing.Interfaces.Context;
 
@@ -8,17 +11,54 @@ namespace CarCare.Processing.Interfaces.Context;
 public interface ILoginContext
 {
     /// <summary>
+    /// An event where the username's value changed.
+    /// </summary>
+    event EventHandler UsernameChanged;
+
+    /// <summary>
     /// The command to log the user in.
     /// </summary>
     ICommand LoginCommand { get; }
 
     /// <summary>
-    /// The username of the user.
+    /// The command to navigate to the Register page.
+    /// </summary>
+    ICommand RegisterCommand { get; }
+
+    /// <summary>
+    /// The command that selects a user.
+    /// </summary>
+    ICommand SelectUserCommand { get; }
+
+    /// <summary>
+    /// The command that returns the view to the users list.
+    /// </summary>
+    ICommand ReturnToUsersListCommand { get; }
+
+    /// <summary>
+    /// A list of all registered users.
+    /// </summary>
+    ObservableCollection<UserDto> Users { get; } 
+
+    /// <summary>
+    /// The username of the selected user.
     /// </summary>
     string Username { get; }
+
+    /// <summary>
+    /// The avatar image of the selected user.
+    /// </summary>
+    BitmapImage? Avatar { get; }
 
     /// <summary>
     /// An error that may have occurred during login.
     /// </summary>
     string Error { get; }
+
+    /// <summary>
+    /// Initialises the data context.
+    /// </summary>
+    /// <returns>An awaitable <see cref="Task"/>.</returns>
+    Task InitialiseAsync();
+
 }
