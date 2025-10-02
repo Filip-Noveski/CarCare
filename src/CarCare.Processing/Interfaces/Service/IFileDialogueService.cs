b03e20c070a507cd3f246acb43ca6de967507116
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-02 17:30:22 +0200

    IFileDialogueService interface [Processing]

---
+++ src/CarCare.Processing/Interfaces/Service/IFileDialogueService.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace CarCare.Processing.Interfaces.Service;
+
+/// <summary>
+/// A service for dealing with file dialogues.
+/// </summary>
+public interface IFileDialogueService
+{
+    /// <summary>
+    /// Opens a dialogue to select and attach a desired image.
+    /// </summary>
+    /// <returns>A <see cref="BitmapImage"/> or null.</returns>
+    BitmapImage? GetImageFile();
+}
