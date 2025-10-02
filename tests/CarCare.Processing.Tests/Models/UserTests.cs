0325c9f366abcab8b7a98cab3ebcb38baecd0f1f
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-02 17:04:31 +0200

    Test updated User logic in UserTests [Processing.Tests]

--- tests/CarCare.Processing.Tests/Models/UserTests.cs
+++ tests/CarCare.Processing.Tests/Models/UserTests.cs
@@ -1,16 +1,22 @@
-﻿using CarCare.Processing.Models;
+﻿using CarCare.Persistence.Models;
+using CarCare.Processing.Interfaces.Service;
+using CarCare.Processing.Models;
 using FluentAssertions;
 using Microsoft.AspNetCore.Identity;
+using NSubstitute;
+using System.Windows.Media.Imaging;
 
 namespace CarCare.Processing.Tests.Models;
 
 public class UserTests
 {
     private readonly PasswordHasher<User> _hasher;
+    private readonly IBitmapCreatorService _bitmapCreator;
 
     public UserTests()
     {
         _hasher = new();
+        _bitmapCreator = Substitute.For<IBitmapCreatorService>();
     }
 
     [Fact]
@@ -19,16 +25,22 @@ public class UserTests
         // Arrange
         string username = "Some username";
         string password = "Some password";
+        BitmapImage avatar = new();
+
+        _bitmapCreator.GetGenericAvatar(username).Returns(avatar);
 
         // Act
-        User user = User.Create(username, password, _hasher);
+        User user = User.Create(username, password, _hasher, _bitmapCreator);
 
         // Assert
         PasswordVerificationResult passVerification = _hasher.VerifyHashedPassword(null!, user.Password, password);
 
         user.Username.Should().Be(username);
-        user.Avatar.Should().BeNull();
+        user.Avatar.Should().Be(avatar);
+        user.HasCustomAvatar.Should().BeFalse();
         passVerification.Should().Be(PasswordVerificationResult.Success);
+
+        _bitmapCreator.Received().GetGenericAvatar(username);
     }
 
     [Fact]
@@ -38,6 +50,31 @@ public class UserTests
         string username = "Some username";
         string password = "Some password";
         byte[] avatar = new byte[] { 0x02, 0x13, 0x65 };
+        BitmapImage bmp = new();
+
+        _bitmapCreator.ConvertToBitmap(avatar).Returns(bmp);
+
+        // Act
+        User user = User.Create(username, password, avatar, _hasher, _bitmapCreator);
+
+        // Assert
+        PasswordVerificationResult passVerification = _hasher.VerifyHashedPassword(null!, user.Password, password);
+
+        user.Username.Should().Be(username);
+        user.Avatar.Should().Be(bmp);
+        user.HasCustomAvatar.Should().BeTrue();
+        passVerification.Should().Be(PasswordVerificationResult.Success);
+
+        _bitmapCreator.Received().ConvertToBitmap(avatar);
+    }
+
+    [Fact]
+    public void ShouldCreateUserWithHashedPasswordAndDirectlyPassesdAvatar()
+    {
+        // Arrange
+        string username = "Some username";
+        string password = "Some password";
+        BitmapImage avatar = new();
 
         // Act
         User user = User.Create(username, password, avatar, _hasher);
@@ -46,7 +83,72 @@ public class UserTests
         PasswordVerificationResult passVerification = _hasher.VerifyHashedPassword(null!, user.Password, password);
 
         user.Username.Should().Be(username);
-        user.Avatar.Should().BeEquivalentTo(avatar);
+        user.Avatar.Should().Be(avatar);
+        user.HasCustomAvatar.Should().BeTrue();
         passVerification.Should().Be(PasswordVerificationResult.Success);
+    }
+
+    [Fact]
+    public void ShouldConvertToDto()
+    {
+        // Arrange
+        Guid id = Guid.NewGuid();
+        string username = "Some username";
+        string password = "Some password";
+        BitmapImage avatar = new();
+        User user = new(id, username, password, avatar, true);
+
+        // Act
+        UserDto dto = user.ToDto();
+
+        // Assert
+        dto.Id.Should().Be(id);
+        dto.Username.Should().Be(username);
+        dto.Avatar.Should().Be(avatar);
+    }
+
+    [Fact]
+    public void ShouldConvertToDaoWithAvatar()
+    {
+        // Arrange
+        Guid id = Guid.NewGuid();
+        string username = "Some username";
+        string password = "Some password";
+        BitmapImage avatar = new();
+        User user = new(id, username, password, avatar, true);
+        byte[] avatarBytes = Array.Empty<byte>();
+
+        _bitmapCreator.ConvertToBinary(avatar).Returns(avatarBytes);
+
+        // Act
+        UserDao dao = user.ToDao(_bitmapCreator);
+
+        // Assert
+        dao.Id.Should().Be(id);
+        dao.Username.Should().Be(username);
+        dao.Password.Should().Be(password);
+        dao.Avatar.Should().BeEquivalentTo(avatarBytes);
+
+        _bitmapCreator.Received().ConvertToBinary(avatar);
+    }
+
+    [Fact]
+    public void ShouldConvertToDaoWithoutAvatar()
+    {
+        // Arrange
+        Guid id = Guid.NewGuid();
+        string username = "Some username";
+        string password = "Some password";
+        BitmapImage avatar = new();
+        User user = new(id, username, password, avatar, false);
+
+        // Act
+        UserDao dao = user.ToDao(_bitmapCreator);
+
+        // Assert
+        dao.Id.Should().Be(id);
+        dao.Username.Should().Be(username);
+        dao.Password.Should().Be(password);
+        dao.Avatar.Should().BeNull();
     }
 }
