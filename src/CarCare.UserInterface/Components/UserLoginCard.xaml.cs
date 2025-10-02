67a12c6d547cb2009a318ffc38304b6458a454ec
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-02 17:35:50 +0200

    UserLoginCard user control [UserInterface]

---
+++ src/CarCare.UserInterface/Components/UserLoginCard.xaml.cs
@@ -0,0 +1,38 @@
+﻿using CarCare.Processing.Models;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CarCare.UserInterface.Components;
+
+/// <summary>
+/// Interaction logic for UserCard.xaml
+/// </summary>
+public partial class UserLoginCard : UserControl
+{
+    public static readonly DependencyProperty SelectUserCommandProperty = DependencyProperty.Register(
+        name: nameof(SelectUserCommand),
+        propertyType: typeof(ICommand),
+        ownerType: typeof(UserLoginCard));
+
+    public ICommand SelectUserCommand
+    {
+        get => (ICommand)GetValue(SelectUserCommandProperty);
+        set => SetValue(SelectUserCommandProperty, value);
+    }
+
+    public UserLoginCard()
+    {
+        InitializeComponent();
+    }
+
+    private void SelectUser(object sender, MouseButtonEventArgs e)
+    {
+        if (DataContext is not UserDto user)
+        {
+            return;
+        }
+
+        SelectUserCommand.Execute(user.Username);
+    }
+}
