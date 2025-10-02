ba1e91d1d3fa22df9509346ede4bf3717f9ccd10
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-02 23:20:37 +0200

    UserControlContextTests [Processing.Tests]

---
+++ tests/CarCare.Processing.Tests/Contexts/UserControlContextTests.cs
@@ -0,0 +1,36 @@
+﻿using CarCare.Processing.Contexts;
+using CarCare.Processing.Interfaces.Context;
+using CarCare.Processing.Interfaces.Service;
+using CarCare.Processing.Interfaces.Session;
+using NSubstitute;
+
+namespace CarCare.Processing.Tests.Contexts;
+
+public class UserControlContextTests
+{
+    private readonly INavigationService _navigationService;
+    private readonly IUserSession _userSession;
+    private readonly UserControlContext _sut;
+
+    public UserControlContextTests()
+    {
+        _navigationService = Substitute.For<INavigationService>();
+        _userSession = Substitute.For<IUserSession>();
+        _sut = new(_navigationService, _userSession);
+    }
+
+    [Fact]
+    public void ShouldRequestUserLogoutAndNavigateToAuthenticationView()
+    {
+        // Arrange
+        _userSession.LogoutUser();
+        _navigationService.NavigateTo<IAuthenticationContext>();
+
+        // Act
+        _sut.LogoutCommand.Execute(null);
+
+        // Assert
+        _userSession.Received().LogoutUser();
+        _navigationService.Received().NavigateTo<IAuthenticationContext>();
+    }
+}
