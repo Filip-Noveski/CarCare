fd4bb02dd5637745f99881f3c54dd9ca248deac1
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-04 12:59:27 +0200

    UserSettingsDao [Persistence]

---
+++ src/CarCare.Persistence/Models/UserSettingsDao.cs
@@ -0,0 +1,12 @@
+﻿namespace CarCare.Persistence.Models;
+
+/// <summary>
+/// A database user settings model.
+/// </summary>
+public class UserSettingsDao
+{
+    /// <summary>
+    /// The id of the user that owns the settings.
+    /// </summary>
+    public Guid UserId { get; set; }
+}
