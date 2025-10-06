2ff2ffa49a40d255bf592ae11831e7c79c0a33b8
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-06 21:57:11 +0200

    Test behaviour with authentication in UserSettingsServiceTests [Processing.Tests]

--- tests/CarCare.Processing.Tests/Services/UserSettingsServiceTests.cs
+++ tests/CarCare.Processing.Tests/Services/UserSettingsServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using CarCare.Persistence.Interfaces;
 using CarCare.Persistence.Models;
+using CarCare.Processing.Interfaces.Session;
 using CarCare.Processing.Models.Core;
 using CarCare.Processing.Services;
 using FluentAssertions;
@@ -10,12 +11,14 @@ namespace CarCare.Processing.Tests.Services;
 public class UserSettingsServiceTests
 {
     private readonly IUserSettingsRepository _settingsRepository;
+    private readonly IUserSession _session;
     private readonly UserSettingsService _sut;
 
     public UserSettingsServiceTests()
     {
         _settingsRepository = Substitute.For<IUserSettingsRepository>();
-        _sut = new(_settingsRepository);
+        _session = Substitute.For<IUserSession>();
+        _sut = new(_settingsRepository, _session);
     }
 
     [Fact]
@@ -35,10 +38,11 @@ public class UserSettingsServiceTests
     }
 
     [Fact]
-    public async Task ShouldRequestSettingsDeletion()
+    public async Task ShouldRequestSettingsDeletionOnAuth()
     {
         // Arrange
         Guid id = Guid.NewGuid();
+        _session.IsAuthenticated(id).Returns(true);
         _settingsRepository.DeleteAsync(id).Returns(Task.CompletedTask);
 
         // Act
@@ -46,6 +50,23 @@ public class UserSettingsServiceTests
 
         // Assert
         await _settingsRepository.Received().DeleteAsync(id);
+        _session.Received().IsAuthenticated(id);
+    }
+
+    [Fact]
+    public async Task ShouldNotRequestSettingsDeletionWithoutAuth()
+    {
+        // Arrange
+        Guid id = Guid.NewGuid();
+        _session.IsAuthenticated(id).Returns(false);
+        _settingsRepository.DeleteAsync(id).Returns(Task.CompletedTask);
+
+        // Act
+        await _sut.DeleteAsync(id);
+
+        // Assert
+        await _settingsRepository.DidNotReceive().DeleteAsync(id);
+        _session.Received().IsAuthenticated(id);
     }
 
     [Fact]
@@ -65,11 +86,12 @@ public class UserSettingsServiceTests
     }
 
     [Fact]
-    public async Task ShouldRequestUpdate()
+    public async Task ShouldRequestUpdateOnAuth()
     {
         // Arrange
         UserSettings settings = new(Guid.NewGuid());
         _settingsRepository.UpdateAsync(Arg.Any<UserSettingsDao>()).Returns(Task.CompletedTask);
+        _session.IsAuthenticated(settings.UserId).Returns(true);
 
         // Act
         await _sut.UpdateAsync(settings);
@@ -77,5 +99,23 @@ public class UserSettingsServiceTests
         // Assert
         await _settingsRepository.Received().UpdateAsync(Arg.Is<UserSettingsDao>(
             x => x.UserId == settings.UserId));
+        _session.Received().IsAuthenticated(settings.UserId);
+    }
+
+    [Fact]
+    public async Task ShouldNotRequestUpdateWithoutAuth()
+    {
+        // Arrange
+        UserSettings settings = new(Guid.NewGuid());
+        _settingsRepository.UpdateAsync(Arg.Any<UserSettingsDao>()).Returns(Task.CompletedTask);
+        _session.IsAuthenticated(settings.UserId).Returns(false);
+
+        // Act
+        await _sut.UpdateAsync(settings);
+
+        // Assert
+        await _settingsRepository.DidNotReceive().UpdateAsync(Arg.Is<UserSettingsDao>(
+            x => x.UserId == settings.UserId));
+        _session.Received().IsAuthenticated(settings.UserId);
     }
 }
