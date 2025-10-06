98fbf716a328bc06f41be1b8a6974c7fc81761c5
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-06 15:24:58 +0200

    UserSettingsTests [Processing.Tests]

---
+++ tests/CarCare.Processing.Tests/Models/UserSettingsTests.cs
@@ -0,0 +1,50 @@
+﻿using CarCare.Persistence.Models;
+using CarCare.Processing.Models.Core;
+using CarCare.Processing.Models.Dto;
+using FluentAssertions;
+
+namespace CarCare.Processing.Tests.Models;
+
+public class UserSettingsTests
+{
+    [Fact]
+    public void ShouldCreateDefaultSettings()
+    {
+        // Arrange
+        Guid id = Guid.NewGuid();
+
+        // Act
+        UserSettings settings = UserSettings.CreateDefault(id);
+
+        // Assert
+        settings.UserId.Should().Be(id);
+    }
+
+    [Fact]
+    public void ShouldMapToDto()
+    {
+        // Arrange
+        Guid id = Guid.NewGuid();
+        UserSettings settings = new(id);
+
+        // Act
+        UserSettingsDto dto = settings.ToDto();
+
+        // Assert
+        dto.UserId.Should().Be(id);
+    }
+
+    [Fact]
+    public void ShouldMapToDao()
+    {
+        // Arrange
+        Guid id = Guid.NewGuid();
+        UserSettings settings = new(id);
+
+        // Act
+        UserSettingsDao dao = settings.ToDao();
+
+        // Assert
+        dao.UserId.Should().Be(id);
+    }
+}
