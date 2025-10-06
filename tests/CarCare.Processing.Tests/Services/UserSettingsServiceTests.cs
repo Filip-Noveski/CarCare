55659b9f9edfe9715b369943df65a3b380119655
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-06 15:25:09 +0200

    UserSettingsServiceTests [Processing.Tests]

---
+++ tests/CarCare.Processing.Tests/Services/UserSettingsServiceTests.cs
@@ -0,0 +1,81 @@
+﻿using CarCare.Persistence.Interfaces;
+using CarCare.Persistence.Models;
+using CarCare.Processing.Models.Core;
+using CarCare.Processing.Services;
+using FluentAssertions;
+using NSubstitute;
+
+namespace CarCare.Processing.Tests.Services;
+
+public class UserSettingsServiceTests
+{
+    private readonly IUserSettingsRepository _settingsRepository;
+    private readonly UserSettingsService _sut;
+
+    public UserSettingsServiceTests()
+    {
+        _settingsRepository = Substitute.For<IUserSettingsRepository>();
+        _sut = new(_settingsRepository);
+    }
+
+    [Fact]
+    public async Task ShouldRequestSettingsCreation()
+    {
+        // Arrange
+        Guid id = Guid.NewGuid();
+        UserSettings settings = new(id);
+        _settingsRepository.AddAsync(Arg.Any<UserSettingsDao>()).Returns(Task.CompletedTask);
+
+        // Act
+        await _sut.AddAsync(settings);
+
+        // Assert
+        await _settingsRepository.Received().AddAsync(Arg.Is<UserSettingsDao>(
+                x => x.UserId == id));
+    }
+
+    [Fact]
+    public async Task ShouldRequestSettingsDeletion()
+    {
+        // Arrange
+        Guid id = Guid.NewGuid();
+        _settingsRepository.DeleteAsync(id).Returns(Task.CompletedTask);
+
+        // Act
+        await _sut.DeleteAsync(id);
+
+        // Assert
+        await _settingsRepository.Received().DeleteAsync(id);
+    }
+
+    [Fact]
+    public async Task ShouldGetSettings()
+    {
+        // Arrange
+        Guid id = Guid.NewGuid();
+        UserSettingsDao dao = new(id);
+        _settingsRepository.GetAsync(id).Returns(dao);
+
+        // Act
+        UserSettings result = await _sut.GetAsync(id);
+
+        // Assert
+        await _settingsRepository.Received().GetAsync(id);
+        result.Should().BeEquivalentTo(new UserSettings(id));
+    }
+
+    [Fact]
+    public async Task ShouldRequestUpdate()
+    {
+        // Arrange
+        UserSettings settings = new(Guid.NewGuid());
+        _settingsRepository.UpdateAsync(Arg.Any<UserSettingsDao>()).Returns(Task.CompletedTask);
+
+        // Act
+        await _sut.UpdateAsync(settings);
+
+        // Assert
+        await _settingsRepository.Received().UpdateAsync(Arg.Is<UserSettingsDao>(
+            x => x.UserId == settings.UserId));
+    }
+}
