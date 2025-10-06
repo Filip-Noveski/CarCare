0084e0c5d0f77b0874f873b7ccbc7f3aa47fc3eb
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-06 15:04:36 +0200

    UserSettings [Processing]

---
+++ src/CarCare.Processing/Models/Core/UserSettings.cs
@@ -0,0 +1,29 @@
+﻿using CarCare.Persistence.Models;
+using CarCare.Processing.Models.Dto;
+
+namespace CarCare.Processing.Models.Core;
+
+internal class UserSettings
+{
+    public Guid UserId { get; set; }
+
+    public UserSettings(Guid userId)
+    {
+        UserId = userId;
+    }
+
+    public UserSettingsDto ToDto()
+    {
+        return new(UserId);
+    }
+
+    public UserSettingsDao ToDao()
+    {
+        return new(UserId);
+    }
+
+    public static UserSettings CreateDefault(Guid userId)
+    {
+        return new(userId);
+    }
+}
