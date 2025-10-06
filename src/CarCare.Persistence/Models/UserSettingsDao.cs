ed1d0afa9beb5944a0d08eb550dbb18b1eb2c18f
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-06 12:12:49 +0200

    Constructors in UserSettingsDao [Persistence]

--- src/CarCare.Persistence/Models/UserSettingsDao.cs
+++ src/CarCare.Persistence/Models/UserSettingsDao.cs
@@ -9,4 +9,21 @@ public class UserSettingsDao
     /// The id of the user that owns the settings.
     /// </summary>
     public Guid UserId { get; set; }
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="UserSettingsDao"/> class.
+    /// </summary>
+    public UserSettingsDao()
+    {
+        UserId = Guid.Empty;
+    }
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="UserSettingsDao"/> class.
+    /// </summary>
+    /// <param name="userId">The id of the user that owns the settings.</param>
+    public UserSettingsDao(Guid userId)
+    {
+        UserId = userId;        
+    }
 }
