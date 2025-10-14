e77064731b30f9c2212147844ff183f4f7b8f2a8
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-14 15:49:40 +0200

    IServiceProviderExtensions [Processing]

---
+++ src/CarCare.UserInterface/Extensions/IServiceProviderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using CarCare.Processing.Interfaces.Service;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarCare.UserInterface.Extensions;
+
+/// <summary>
+/// Provides extension to <see cref="IServiceProvider"/>.
+/// </summary>
+public static class IServiceProviderExtensions
+{
+    /// <summary>
+    /// Initialises the application state.
+    /// </summary>
+    /// <param name="serviceProvider">The <see cref="IServiceProvider"/> of the app.</param>
+    public static void InitialiseApplication(this IServiceProvider serviceProvider)
+    {
+        // set the default theme
+        IThemeService themeService = serviceProvider.GetRequiredService<IThemeService>();
+        themeService.SetDefaultTheme();
+    }
+}
