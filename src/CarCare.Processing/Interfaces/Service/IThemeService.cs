447cf43044d577d21ef07ea5daa3a46f122d189a
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-14 15:16:59 +0200

    IThemeService [Processing]

---
+++ src/CarCare.Processing/Interfaces/Service/IThemeService.cs
@@ -0,0 +1,20 @@
+﻿using CarCare.Processing.Enums;
+
+namespace CarCare.Processing.Interfaces.Service;
+
+/// <summary>
+/// A service for managing application themes.
+/// </summary>
+public interface IThemeService
+{
+    /// <summary>
+    /// The currently selected <see cref="ApplicationTheme"/>.
+    /// </summary>
+    ApplicationTheme Theme { get; }
+
+    /// <summary>
+    /// Changes the application theme to the provided <paramref name="theme"/>.
+    /// </summary>
+    /// <param name="theme">The <see cref="ApplicationTheme"/> to set.</param>
+    void ChangeTheme(ApplicationTheme theme);
+}
