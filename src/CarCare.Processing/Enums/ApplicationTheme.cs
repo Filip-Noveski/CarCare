40bc8110622ee478d54c2c737ed01a059890c371
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-14 14:42:31 +0200

    ApplicationTheme enum [Processing]

---
+++ src/CarCare.Processing/Enums/ApplicationTheme.cs
@@ -0,0 +1,17 @@
+﻿namespace CarCare.Processing.Enums;
+
+/// <summary>
+/// A colour theme that the application may use.
+/// </summary>
+public enum ApplicationTheme
+{
+    /// <summary>
+    /// A dark colour theme.
+    /// </summary>
+    Dark,
+
+    /// <summary>
+    /// A light colour theme.
+    /// </summary>
+    Light
+}
