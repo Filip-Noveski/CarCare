85bca2147a62ccc9856f38bdc952abee72f68903
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-18 14:55:55 +0200

    LoadingSpinner control (borrowed from sister project) [UserInterface]

---
+++ src/CarCare.UserInterface/Controls/LoadingSpinner.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace CarCare.UserInterface.Controls;
+
+/// <summary>
+/// Interaction logic for LoadingSpinner.xaml
+/// </summary>
+public partial class LoadingSpinner : UserControl
+{
+    public LoadingSpinner()
+    {
+        InitializeComponent();
+    }
+}
