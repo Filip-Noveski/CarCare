d77b8702a4b334ad76390f9b7232c59f7c06d2d6
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-19 16:55:29 +0200

    SettingsCog user control [UserInterface]

---
+++ src/CarCare.UserInterface/Controls/SettingsCog.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace CarCare.UserInterface.Controls;
+
+/// <summary>
+/// Interaction logic for SettingsCog.xaml
+/// </summary>
+public partial class SettingsCog : UserControl
+{
+    public SettingsCog()
+    {
+        InitializeComponent();
+    }
+}
