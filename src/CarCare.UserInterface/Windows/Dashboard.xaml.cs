00f2b7167f371a1d6f2ca29e11dffee85595d227
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-10-19 16:55:59 +0200

    General layout on Dashboard [UserInterface]

--- src/CarCare.UserInterface/Windows/Dashboard.xaml.cs
+++ src/CarCare.UserInterface/Windows/Dashboard.xaml.cs
@@ -1,6 +1,8 @@
-﻿using CarCare.UserInterface.Components;
+﻿using CarCare.Processing.Interfaces.Context;
+using CarCare.UserInterface.Components;
 using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using System.Windows.Shell;
 
 namespace CarCare.UserInterface.Windows;
 
@@ -12,10 +14,21 @@ public partial class Dashboard : Window
     /// <summary>
     /// Creates a new instance of the <see cref="Dashboard"/> class.
     /// </summary>
-    public Dashboard(IServiceProvider serviceProvider)
+    public Dashboard(IWindowContext context, IServiceProvider serviceProvider)
     {
+        DataContext = context;
         InitializeComponent();
         SessionControl sessionControl = serviceProvider.GetRequiredService<SessionControl>();
-        MainGrid.Children.Add(sessionControl);
+        WindowChrome.SetIsHitTestVisibleInChrome(sessionControl, true);
+        SessionControlContainer.Children.Add(sessionControl);
+    }
+
+    private void WindowStateChanged(object sender, EventArgs e)
+    {
+        ContentGrid.Margin = WindowState switch
+        {
+            WindowState.Maximized => new(8),
+            _ => new(0)
+        };
     }
 }
