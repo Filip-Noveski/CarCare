15ac0f3b4c291dfa06f9c32f28be0be53ae827dc
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-11-15 17:32:37 +0100

    SettingsTab [Processing]

---
+++ src/CarCare.Processing/Enums/SettingsTab.cs
@@ -0,0 +1,17 @@
+﻿namespace CarCare.Processing.Enums;
+
+/// <summary>
+/// A tab in the settings window.
+/// </summary>
+public enum SettingsTab
+{
+    /// <summary>
+    /// The account settings tab.
+    /// </summary>
+    AccountSettings = 0,
+
+    /// <summary>
+    /// The application settings tab.
+    /// </summary>
+    ApplicationSettings = 1
+}
