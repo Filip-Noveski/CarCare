5aac2d268fa613db68b012914c9da766e54cbe84
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-11-15 17:38:18 +0100

    Rename AccountSettings window to Settings [UserInterface]

---
+++ src/CarCare.UserInterface/Windows/Settings.xaml.cs
@@ -0,0 +1,24 @@
+﻿using CarCare.Processing.Interfaces.Context;
+using CarCare.UserInterface.Components;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace CarCare.UserInterface.Windows;
+
+/// <summary>
+/// Interaction logic for AccountSettings.xaml
+/// </summary>
+public partial class Settings : Window
+{
+    private readonly ISettingsContext _context;
+
+    public Settings(ISettingsContext context, IServiceProvider provider)
+    {
+        DataContext = context;
+        _context = context;
+        InitializeComponent();
+
+        AccountSettings accSettings = provider.GetRequiredService<AccountSettings>();
+        AccountSettingsTab.Content = accSettings;
+    }
+}
