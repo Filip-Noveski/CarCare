6c2327553f034f7c0b9119588ae23020c9d14c5f
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-11-15 17:33:44 +0100

    Add OpenAppSettings command to DashboardContext [Processing]

--- src/CarCare.Processing/Interfaces/Context/IDashboardContext.cs
+++ src/CarCare.Processing/Interfaces/Context/IDashboardContext.cs
@@ -1,8 +1,14 @@
-﻿namespace CarCare.Processing.Interfaces.Context;
+﻿using System.Windows.Input;
+
+namespace CarCare.Processing.Interfaces.Context;
 
 /// <summary>
 /// A data context for the main Dashboard window of the application.
 /// </summary>
-public interface IDashboardContext
+public interface IDashboardContext : IWindowContext
 {
+    /// <summary>
+    /// Opens the settings window with the App Settings tab displayed.
+    /// </summary>
+    ICommand OpenAppSettingsCommand { get; }
 }
