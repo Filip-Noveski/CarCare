94adbb2273ccb74bafc4b552d74f1f4aec11353a
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-11-15 21:51:22 +0100

    AsyncCommand [Processing]

---
+++ src/CarCare.Processing/Commands/AsyncCommand.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows.Input;
+
+namespace CarCare.Processing.Commands;
+
+internal class AsyncCommand : ICommand
+{
+    private readonly Func<Task> _execute;
+    private readonly Predicate<object?>? _canExecute;
+    private bool _isExecuting;
+
+    public AsyncCommand(Func<Task> execute)
+    {
+        _execute = execute;
+        _isExecuting = false;
+    }
+
+    public AsyncCommand(Func<Task> execute, Predicate<object?>? canExecute)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+        _isExecuting = false;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return !_isExecuting && (_canExecute == null || _canExecute(parameter));
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    public async void Execute(object? parameter)
+    {
+        _isExecuting = true;
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        try
+        {
+            await _execute();
+        }
+        finally
+        {
+            _isExecuting = false;
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
