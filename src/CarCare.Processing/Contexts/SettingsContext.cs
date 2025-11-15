99196aac868ed10c31a990caf8a69ff0e3b9b9a6
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-11-15 17:33:12 +0100

    Add tab to ISettingsContext [Processing]

--- src/CarCare.Processing/Contexts/SettingsContext.cs
+++ src/CarCare.Processing/Contexts/SettingsContext.cs
@@ -1,8 +1,17 @@
-﻿using CarCare.Processing.Interfaces.Context;
+﻿using CarCare.Processing.Enums;
+using CarCare.Processing.Interfaces.Context;
 
 namespace CarCare.Processing.Contexts;
 
 internal class SettingsContext : WindowContext, ISettingsContext
 {
-
+    public SettingsTab Tab 
+    { 
+        get => field;
+        set
+        {
+            field = value;
+            OnPropertyChanged();
+        }
+    }
 }
--- src/CarCare.Processing/Interfaces/Context/ISettingsContext.cs
+++ src/CarCare.Processing/Interfaces/Context/ISettingsContext.cs
@@ -1,9 +1,14 @@
-﻿namespace CarCare.Processing.Interfaces.Context;
+﻿using CarCare.Processing.Enums;
+
+namespace CarCare.Processing.Interfaces.Context;
 
 /// <summary>
 /// Data context for the Account Settings window.
 /// </summary>
 public interface ISettingsContext : IWindowContext
 {
-
+    /// <summary>
+    /// The selected settings tab.
+    /// </summary>
+    SettingsTab Tab { get; set; }
 }
