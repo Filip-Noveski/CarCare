21dd02bd31a7e6ea4b8f6e027234c54e7fd9d06b
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-11-15 17:33:55 +0100

    DashboardContext implementation [Processing]

---
+++ src/CarCare.Processing/Contexts/DashboardContext.cs
@@ -0,0 +1,30 @@
+﻿using CarCare.Processing.Commands;
+using CarCare.Processing.Enums;
+using CarCare.Processing.Interfaces.Context;
+using CarCare.Processing.Interfaces.Service;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Input;
+
+namespace CarCare.Processing.Contexts;
+
+internal class DashboardContext : WindowContext, IDashboardContext
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly INavigationService _navigationService;
+
+    public ICommand OpenAppSettingsCommand { get; }
+
+    public DashboardContext(IServiceProvider serviceProvider, INavigationService navigationService)
+    {
+        _serviceProvider = serviceProvider;
+        _navigationService = navigationService;
+        OpenAppSettingsCommand = new Command(OpenAppSettings);
+    }
+
+    private void OpenAppSettings(object? parameter)
+    {
+        _navigationService.NavigateTo<ISettingsContext>();
+        ISettingsContext settingsContext = _serviceProvider.GetRequiredService<ISettingsContext>();
+        settingsContext.Tab = SettingsTab.ApplicationSettings;
+    }
+}
