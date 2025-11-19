2679a01f1290f6c60e4b0f9f9609e6a756bb8ebe
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-11-19 13:34:10 +0100

    ICurrencyRatesService [Api]

---
+++ src/CarCare.Api/Interfaces/ICurrencyRatesService.cs
@@ -0,0 +1,28 @@
+﻿using CarCare.Api.Enums;
+using CarCare.Api.Models.Dao;
+
+namespace CarCare.Api.Interfaces;
+
+/// <summary>
+/// Manages exchange rates between currencies.
+/// </summary>
+public interface ICurrencyRatesService
+{
+    /// <summary>
+    /// Converts the <paramref name="amount"/> to the provided <paramref name="currency"/>.
+    /// </summary>
+    /// <param name="amount">The <see cref="FinancialAmountDao"/> to convert.</param>
+    /// <param name="currency">The <see cref="Currency"/> to convert to.</param>
+    /// <returns>A new <see cref="FinancialAmountDao"/> object.</returns>
+    Task<FinancialAmountDao> ConvertAsync(FinancialAmountDao amount, Currency currency);
+
+    /// <summary>
+    /// Converts the <paramref name="amount"/> to the provided <paramref name="currency"/>
+    /// with rates for the provided <paramref name="date"/>.
+    /// </summary>
+    /// <param name="amount">The <see cref="FinancialAmountDao"/> to convert.</param>
+    /// <param name="currency">The <see cref="Currency"/> to convert to.</param>
+    /// <param name="date">The <see cref="DateOnly"/> to fetch rates for.</param>
+    /// <returns>A new <see cref="FinancialAmountDao"/> object.</returns>
+    Task<FinancialAmountDao> ConvertAsync(FinancialAmountDao amount, Currency currency, DateOnly date);
+}
