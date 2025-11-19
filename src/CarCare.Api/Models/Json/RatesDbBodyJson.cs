72e034de831cd4f9f31f09599fc8d0124c028efc
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-11-19 13:34:00 +0100

    RatesDbBodyJson [Api]

---
+++ src/CarCare.Api/Models/Json/RatesDbBodyJson.cs
@@ -0,0 +1,15 @@
+﻿namespace CarCare.Api.Models.Json;
+
+internal class RatesDbBodyJson
+{
+    public RatesDbDataJson Data { get; set; } = null!;
+}
+
+internal class RatesDbDataJson
+{
+    public DateTime Date { get; set; }
+
+    public string From { get; set; } = null!;
+
+    public Dictionary<string, double> Rates { get; set; } = new();
+}
