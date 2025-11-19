8952aa67cc6367ccc7907dc263a55d846baa193f
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-11-19 13:33:15 +0100

    Currency enum [Api]

---
+++ src/CarCare.Api/Enums/Currency.cs
@@ -0,0 +1,22 @@
+﻿namespace CarCare.Api.Enums;
+
+/// <summary>
+/// Represents a financial currency.
+/// </summary>
+public enum Currency
+{
+    /// <summary>Macedonian denar.</summary>
+    Mkd,
+
+    /// <summary>European Euro.</summary>
+    Eur,
+    
+    /// <summary>Australian dollar.</summary>
+    Aud,
+    
+    /// <summary>American dollar.</summary>
+    Usd,
+
+    /// <summary>British pound.</summary>
+    Gbp
+}
