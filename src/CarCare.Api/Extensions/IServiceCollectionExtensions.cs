ef836cbb70b1c9c9819c9bea2eb9e97eb45dd80c
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-11-19 13:34:35 +0100

    RatesDbCurrencyRatesService implementation and registration [Api]

---
+++ src/CarCare.Api/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using CarCare.Api.Interfaces;
+using CarCare.Api.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarCare.Api.Extensions;
+
+/// <summary>
+/// Provides extensions to the <see cref="IServiceCollection"/> interface.
+/// </summary>
+public static class IServiceCollectionExtensions
+{
+    /// <summary>
+    /// Registers all API level services.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+    public static void AddApiServices(this IServiceCollection services)
+    {
+        services.AddHttpClient<RatesDbCurrencyRatesService>(
+            c => c.BaseAddress = new("https://free.ratesdb.com/v1"));
+        services.AddScoped<ICurrencyRatesService, RatesDbCurrencyRatesService>();
+    }
+}
---
+++ src/CarCare.Api/Services/RatesDbCurrencyRatesService.cs
@@ -0,0 +1,48 @@
+﻿using CarCare.Api.Enums;
+using CarCare.Api.Interfaces;
+using CarCare.Api.Models.Dao;
+using CarCare.Api.Models.Json;
+using System.Net.Http.Json;
+
+namespace CarCare.Api.Services;
+
+internal class RatesDbCurrencyRatesService : ICurrencyRatesService
+{
+    private readonly HttpClient _client;
+
+    public RatesDbCurrencyRatesService(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<FinancialAmountDao> ConvertAsync(FinancialAmountDao amount, Currency currency)
+    {
+        string from = $"from={amount.Currency.ToString().ToUpper()}";
+        string to = $"to={currency.ToString().ToUpper()}";
+        string endpoint = $"/rates?{from}&{to}";
+
+        double rate = await Call(endpoint);
+        double newAmount = amount.Amount * rate;
+        return new(newAmount, currency);
+    }
+
+    public async Task<FinancialAmountDao> ConvertAsync(FinancialAmountDao amount, Currency currency, DateOnly date)
+    {
+        string from = $"from={amount.Currency.ToString().ToUpper()}";
+        string to = $"to={currency.ToString().ToUpper()}";
+        string dateP = $"date={date:yyyy-mm-dd}";
+        string endpoint = $"/rates?{from}&{to}&{dateP}";
+
+        double rate = await Call(endpoint);
+        double newAmount = amount.Amount * rate;
+        return new(newAmount, currency);
+    }
+
+    private async Task<double> Call(string endpoint)
+    {
+        RatesDbBodyJson response = await _client.GetFromJsonAsync<RatesDbBodyJson>(endpoint)
+            ?? throw new InvalidOperationException("The returned JSON was null");
+
+        return response.Data.Rates.First().Value;   // only one rate should be returned
+    }
+}
