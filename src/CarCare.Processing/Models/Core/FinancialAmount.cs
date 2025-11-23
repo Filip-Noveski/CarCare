fbd101fbafe983f969dd9f471c9b9b5a1db2cbae
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-11-23 22:42:23 +0100

    FinancialAmount model [Processing]

---
+++ src/CarCare.Processing/Models/Core/FinancialAmount.cs
@@ -0,0 +1,16 @@
+﻿using CarCare.Processing.Enums;
+
+namespace CarCare.Processing.Models.Core;
+
+internal class FinancialAmount
+{
+    public double Amount { get; set; }
+
+    public Currency Currency { get; set; }
+
+    public FinancialAmount(double amount, Currency currency)
+    {
+        Amount = amount;
+        Currency = currency;
+    }
+}
