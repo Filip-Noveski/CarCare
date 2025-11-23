f414ef6dc7f78239e3d6607bc70e681ea4ad5834
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-11-23 22:40:43 +0100

    CurrencyRate model [Processing]

---
+++ src/CarCare.Processing/Models/Core/CurrencyRate.cs
@@ -0,0 +1,33 @@
+﻿using CarCare.Persistence.Models;
+using CarCare.Processing.Enums;
+
+namespace CarCare.Processing.Models.Core;
+
+internal class CurrencyRate
+{
+    public Currency FromCurrency { get; set; }
+
+    public Currency ToCurrency { get; set; }
+
+    public double Rate { get; set; }
+
+    public DateOnly Date { get; set; }
+
+    public CurrencyRate(
+        Currency fromCurrency, Currency toCurrency, double rate, DateOnly date)
+    {
+        FromCurrency = fromCurrency;
+        ToCurrency = toCurrency;
+        Rate = rate;
+        Date = date;
+    }
+
+    public CurrencyRateDao ToDao()
+    {
+        return new CurrencyRateDao(
+            FromCurrency.ToString().ToUpper(),
+            ToCurrency.ToString().ToUpper(),
+            Rate,
+            Date);
+    }
+}
