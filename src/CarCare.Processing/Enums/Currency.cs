4c5d7c8fe18fef683e78b519621ba38f0f3f6858
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-11-23 22:39:44 +0100

    Currency enum [Processing]

---
+++ src/CarCare.Processing/Enums/Currency.cs
@@ -0,0 +1,25 @@
+﻿namespace CarCare.Processing.Enums;
+
+/// <summary>
+/// A currency for financial data.
+/// </summary>
+public enum Currency
+{
+    /// <summary>Euro.</summary>
+    Eur,
+    
+    /// <summary>Macedonian Denar.</summary>
+    Mkd,
+
+    /// <summary>British Pount.</summary>
+    Gbp,
+
+    /// <summary>American Dollar.</summary>
+    Usd,
+
+    /// <summary>Australian Dollar.</summary>
+    Aud,
+
+    /// <summary>Russian Ruble.</summary>
+    Rub
+}
