b98684640acc8c0c5bf31e2853eb256a6d27adb8
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-11-24 21:50:18 +0100

    CarRepository interface and implementation [Processing]

---
+++ src/CarCare.Persistence/Interfaces/ICarRepository.cs
@@ -0,0 +1,48 @@
+﻿using CarCare.Persistence.Models;
+
+namespace CarCare.Persistence.Interfaces;
+
+/// <summary>
+/// A repository for performing CRUD operations with the
+/// Cars table.
+/// </summary>
+public interface ICarRepository
+{
+    /// <summary>
+    /// Adds a new <paramref name="car"/> entry.
+    /// </summary>
+    /// <param name="car">The <see cref="CarDao"/> to add.</param>
+    /// <returns>An awaitable <see cref="Task"/>.</returns>
+    Task AddAsync(CarDao car);
+
+    /// <summary>
+    /// Deletes the car with the provided <paramref name="id"/>.
+    /// </summary>
+    /// <param name="id">The id to delete by.</param>
+    /// <returns>An awaitable <see cref="Task"/>.</returns>
+    Task DeleteAsync(Guid id);
+
+    /// <summary>
+    /// Gets the <see cref="CarDao"/> by the <paramref name="id"/>.
+    /// </summary>
+    /// <param name="id">The id to search by.</param>
+    /// <returns></returns>
+    Task<CarDao> GetAsync(Guid id);
+
+    /// <summary>
+    /// Gets all <see cref="CarDao"/> entries registered by the user with
+    /// the provided <paramref name="userId"/>.
+    /// </summary>
+    /// <param name="userId">The id of the used to search by.</param>
+    /// <param name="page">The page to display, starting from 1.</param>
+    /// <param name="count">The number of items per page.</param>
+    /// <returns>An <see cref="IEnumerable{T}"/> of <see cref="CarDao"/>.</returns>
+    Task<IEnumerable<CarDao>> GetAllByUserAsync(Guid userId, int page = 1, int count = 10);
+
+    /// <summary>
+    /// Updates the provided <see cref="CarDao"/> entry.
+    /// </summary>
+    /// <param name="car">The car with the new data.</param>
+    /// <returns>An awaitable <see cref="Task"/>.</returns>
+    Task UpdateAsync(CarDao car);
+}
---
+++ src/CarCare.Persistence/Services/CarRepository.cs
@@ -0,0 +1,92 @@
+﻿using CarCare.Persistence.Configuration;
+using CarCare.Persistence.Interfaces;
+using CarCare.Persistence.Models;
+using Dapper;
+using Microsoft.Data.Sqlite;
+
+namespace CarCare.Persistence.Services;
+
+internal class CarRepository : ICarRepository
+{
+    private const string TableName = "Cars";
+    private readonly DBContext _context;
+
+    public CarRepository(DBContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(CarDao car)
+    {
+        using SqliteConnection connection = await _context.CreateConnectionAsync();
+
+        string sql = $"""
+            INSERT INTO {TableName} (Id, UserId, Manufacturer, Model, Specification, ModelYear, Image)
+            VALUES (@Id, @UserId, @Manufacturer, @Model, @Specification, @ModelYear, @Image);
+            """;
+
+        await connection.ExecuteAsync(sql, car);
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        using SqliteConnection connection = await _context.CreateConnectionAsync();
+
+        string sql = $"""
+            DELETE FROM {TableName}
+            WHERE Id = @Id
+            """;
+
+        await connection.ExecuteAsync(sql, new { Id = id });
+    }
+
+    public async Task<IEnumerable<CarDao>> GetAllByUserAsync(Guid userId, int page = 0, int count = 10)
+    {
+        using SqliteConnection connection = await _context.CreateConnectionAsync();
+
+        string sql = $"""
+            SELECT * FROM {TableName}
+            WHERE UserId = @UserId
+            LIMIT @Count OFFSET @Skip
+            """;
+
+        IEnumerable<CarDao> result = await connection.QueryAsync<CarDao>(sql, new
+        {
+            UserId = userId,
+            Count = count,
+            Skip = (page - 1) * count
+        });
+        return result;
+    }
+
+    public async Task<CarDao> GetAsync(Guid id)
+    {
+        using SqliteConnection connection = await _context.CreateConnectionAsync();
+
+        string sql = $"""
+            SELECT * FROM {TableName}
+            WHERE Id = @Id
+            """;
+
+        CarDao result = await connection.QuerySingleAsync<CarDao>(sql, new { Id = id });
+        return result;
+    }
+
+    public async Task UpdateAsync(CarDao car)
+    {
+        using SqliteConnection connection = await _context.CreateConnectionAsync();
+
+        string sql = $"""
+            UPDATE {TableName}
+            SET
+                Manufacturer = @Manufacturer,
+                Model = @Model,
+                Specification = @Specification,
+                ModelYear = @ModelYear,
+                Image = @Image
+            WHERE Id = @Id
+            """;
+
+        await connection.ExecuteAsync(sql, car);
+    }
+}
