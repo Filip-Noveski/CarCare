2ede0706169d64d5b3a73ac7fb6799655ad18a52
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-11-24 21:49:38 +0100

    Add Cars table to _migration.20251125.sql [Persistence]

--- src/CarCare.Persistence/Migrations/_migration.20251125.sql
+++ src/CarCare.Persistence/Migrations/_migration.20251125.sql
@@ -1,6 +1,7 @@
 ﻿-- Migration date: 25.11.2025
 -- Add UserSettings.PreferredCurrency column
 -- Add CurrencyRatesCache table
+-- Add Cars table
 
 ALTER TABLE [UserSettings]
 ADD COLUMN [PreferredCurrency] TEXT NOT NULL DEFAULT 'EUR';
@@ -12,4 +13,16 @@ CREATE TABLE IF NOT EXISTS [CurrencyRatesCache] (
     [Rate] NUMERIC NOT NULL,
     [Date] TEXT NOT NULL,
     [CachedAt] TEXT NOT NULL DEFAULT (DATETIME('now'))
+);
+
+CREATE TABLE IF NOT EXISTS [Cars] (
+    [Id] TEXT PRIMARY KEY,
+    [UserId] TEXT,
+    [Manufacturer] TEXT NOT NULL,
+    [Model] TEXT NOT NULL,
+    [Specification] TEXT NOT NULL,
+    [ModelYear] INTEGER NOT NULL,
+    [Image] BLOB,
+
+    FOREIGN KEY ([UserId]) REFERENCES [Users]([Id])
 );
