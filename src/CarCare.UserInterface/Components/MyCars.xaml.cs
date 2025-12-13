59f77920b85513f02bf44df3d589e20e5fc3a245
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-12-13 15:21:15 +0100

    MyCars view component [UserInterface]

---
+++ src/CarCare.UserInterface/Components/MyCars.xaml.cs
@@ -0,0 +1,19 @@
+﻿using CarCare.Processing.Interfaces.Context;
+using System.Windows.Controls;
+
+namespace CarCare.UserInterface.Components;
+
+/// <summary>
+/// Interaction logic for MyCars.xaml
+/// </summary>
+public partial class MyCars : UserControl
+{
+    private readonly IMyCarsContext _context;
+
+    public MyCars(IMyCarsContext context)
+    {
+        _context = context;
+        DataContext = context;
+        InitializeComponent();
+    }
+}
