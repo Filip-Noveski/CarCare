4179634dd384536361651d6a828210ef8afd7dea
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-12-13 15:19:09 +0100

    AddCarContextTests [Processing.Tests]

---
+++ tests/CarCare.Processing.Tests/Contexts/AddCarContextTests.cs
@@ -0,0 +1,134 @@
+﻿using CarCare.Processing.Contexts;
+using CarCare.Processing.Interfaces.Service;
+using CarCare.Processing.Interfaces.Session;
+using CarCare.Processing.Models.Core;
+using CarCare.Processing.Models.Dto;
+using FluentAssertions;
+using NSubstitute;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace CarCare.Processing.Tests.Contexts;
+
+public class AddCarContextTests
+{
+    private readonly ICarService _carService;
+    private readonly IUserSession _session;
+    private readonly IEventManagerService _eventManager;
+    private readonly IFileDialogueService _fileService;
+    private readonly AddCarContext _sut;
+
+    public AddCarContextTests()
+    {
+        _carService = Substitute.For<ICarService>();
+        _session = Substitute.For<IUserSession>();
+        _eventManager = Substitute.For<IEventManagerService>();
+        _fileService = Substitute.For<IFileDialogueService>();
+        _sut = new(_carService, _session, _eventManager, _fileService);
+    }
+
+    [Fact]
+    public async Task ShouldAddNewCar()
+    {
+        // Arrange
+        _sut.Manufacturer = "Renault";
+        _sut.Model = "Clio";
+        _sut.Specification = "RS 197";
+        _sut.ModelYear = "2008";
+        UserDto user = new(Guid.NewGuid(), string.Empty, null!);
+        _session.User.Returns(user);
+        _carService.AddAsync(Arg.Any<Car>()).Returns(Task.CompletedTask);
+        _eventManager.OnMyCarsChanged();
+
+        // Act
+        Thread thread = new(() => _sut.AddCarCommand.Execute(new Window()));
+        thread.SetApartmentState(ApartmentState.STA);
+        thread.Start();
+        thread.Join();
+
+        // Assert
+        await _carService.Received().AddAsync(Arg.Is<Car>(
+            x => x.Id != Guid.Empty
+            && x.UserId == user.Id
+            && x.Manufacturer == "Renault"
+            && x.Model == "Clio"
+            && x.Specification == "RS 197"
+            && x.ModelYear == 2008
+            && x.Image == null));
+        _eventManager.Received().OnMyCarsChanged();
+    }
+
+    [Fact]
+    public async Task ShouldFailOnManufacturer()
+    {
+        // Arrange
+        _sut.Manufacturer = "";
+        _sut.Model = "Clio";
+        _sut.Specification = "RS 197";
+        _sut.ModelYear = "2008";
+        UserDto user = new(Guid.NewGuid(), string.Empty, null!);
+        _session.User.Returns(user);
+
+        // Act
+        Thread thread = new(() => _sut.AddCarCommand.Execute(new Window()));
+        thread.SetApartmentState(ApartmentState.STA);
+        thread.Start();
+        thread.Join();
+
+        // Assert
+        await _carService.DidNotReceive().AddAsync(Arg.Any<Car>());
+        _eventManager.DidNotReceive().OnMyCarsChanged();
+        _sut.ManufacturerError.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task ShouldFailOnModel()
+    {
+        // Arrange
+        _sut.Manufacturer = "Renault";
+        _sut.Model = "";
+        _sut.Specification = "RS 197";
+        _sut.ModelYear = "2008";
+        UserDto user = new(Guid.NewGuid(), string.Empty, null!);
+        _session.User.Returns(user);
+
+        // Act
+        Thread thread = new(() => _sut.AddCarCommand.Execute(new Window()));
+        thread.SetApartmentState(ApartmentState.STA);
+        thread.Start();
+        thread.Join();
+
+        // Assert
+        await _carService.DidNotReceive().AddAsync(Arg.Any<Car>());
+        _eventManager.DidNotReceive().OnMyCarsChanged();
+        _sut.ModelError.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public void ShouldOpenFileDialogue()
+    {
+        // Arrange
+        BitmapImage image = new();
+        _fileService.GetImageFile().Returns(image);
+
+        // Act
+        _sut.ChooseMainImageCommand.Execute(null);
+
+        // Assert
+        _fileService.Received().GetImageFile();
+        _sut.MainImage.Should().Be(image);
+    }
+
+    [Fact]
+    public void ShouldDeleteMainImage()
+    {
+        // Arrange
+        _sut.MainImage = new();
+
+        // Act
+        _sut.DeleteMainImageCommand.Execute(null);
+
+        // Assert
+        _sut.MainImage.Should().BeNull();
+    }
+}
