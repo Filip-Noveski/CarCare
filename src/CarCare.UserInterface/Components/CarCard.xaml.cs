ac47ea5bbfe097d96435e652f61a8c0ac891d54e
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-12-13 15:20:48 +0100

    CarCard view component [UserInterface]

---
+++ src/CarCare.UserInterface/Components/CarCard.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace CarCare.UserInterface.Components;
+
+/// <summary>
+/// Interaction logic for CarCard.xaml
+/// </summary>
+public partial class CarCard : UserControl
+{
+    public CarCard()
+    {
+        InitializeComponent();
+    }
+}
