c4d23866ebcbaeddb0a6dde38cc45e6b47845d4e
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-12-15 15:30:11 +0100

    RequestsController [Api.MockApi]

---
+++ test-support/CarCare.Api.MockApi/Controllers/RequestsController.cs
@@ -0,0 +1,27 @@
+﻿using CarCare.Api.MockApi.Models;
+using CarCare.Api.MockApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarCare.Api.MockApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RequestsController : ControllerBase
+{
+    private readonly ILogger<RequestsController> _logger;
+    private readonly RequestsService _requestsService;
+
+    public RequestsController(ILogger<RequestsController> logger, RequestsService requestsService)
+    {
+        _logger = logger;
+        _requestsService = requestsService;
+    }
+
+    [HttpGet]
+    public ActionResult<Request> Get()
+    {
+        _logger.LogInformation("Received GET request for mock request");
+        var request = _requestsService.DequeueRequest();
+        return Ok(request);
+    }
+}
