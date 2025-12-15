760f176c15996f6c79c8cc0eda7456d34f1adf7d
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-12-15 15:29:02 +0100

    MockConfigService [Api.MockApi]

---
+++ test-support/CarCare.Api.MockApi/Services/MockConfigService.cs
@@ -0,0 +1,28 @@
+﻿namespace CarCare.Api.MockApi.Services;
+
+public class MockConfigService<T>
+{
+    private readonly Queue<MockConfig<T>> _responses;
+
+    public MockConfigService()
+    {
+        _responses = new();
+    }
+
+    public void EnqueueResponse(T response, int statusCode)
+    {
+        _responses.Enqueue(new MockConfig<T>(response, statusCode));
+    }
+
+    public MockConfig<T> DequeueResponse()
+    {
+        if (_responses.Count == 0)
+        {
+            throw new InvalidOperationException("No mock responses available.");
+        }
+
+        return _responses.Dequeue();
+    }
+}
+
+public sealed record MockConfig<T>(T Response, int StatusCode);
