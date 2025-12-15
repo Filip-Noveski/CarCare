2d28cb8e2d9159e12ca8624421912505f3872a33
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-12-15 15:33:00 +0100

    Request test model [Api.Tests]

---
+++ tests/CarCare.Api.Tests/TestModels/Request.cs
@@ -0,0 +1,8 @@
+﻿namespace CarCare.Api.Tests.TestModels;
+
+internal class Request
+{
+    public string Endpoint { get; set; } = string.Empty;
+
+    public Dictionary<string, string> QueryParameters { get; set; } = new();
+}
