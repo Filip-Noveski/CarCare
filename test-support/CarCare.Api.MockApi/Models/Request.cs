772dd22df35ad0b3961a6bc0b64492732e41552b
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-12-15 15:29:21 +0100

    Request model [Api.MockApi]

---
+++ test-support/CarCare.Api.MockApi/Models/Request.cs
@@ -0,0 +1,8 @@
+﻿namespace CarCare.Api.MockApi.Models;
+
+public class Request
+{
+    public string Endpoint { get; set; } = string.Empty;
+
+    public Dictionary<string, string> QueryParameters { get; set; } = new();
+}
