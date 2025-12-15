eacea4f39344896585340db795d8d4fd94e80261
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-12-15 15:30:56 +0100

    RatesMockConfigController [Api.MockApi]

---
+++ test-support/CarCare.Api.MockApi/Controllers/RatesMockConfigController.cs
@@ -0,0 +1,29 @@
+﻿using CarCare.Api.MockApi.Models;
+using CarCare.Api.MockApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarCare.Api.MockApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RatesMockConfigController : ControllerBase
+{
+    private readonly ILogger<RatesMockConfigController> _logger;
+    private readonly MockConfigService<Rate> _mockConfigService;
+
+    public RatesMockConfigController(
+        ILogger<RatesMockConfigController> logger,
+        MockConfigService<Rate> mockConfigService)
+    {
+        _logger = logger;
+        _mockConfigService = mockConfigService;
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] Rate rate, [FromQuery] int status)
+    {
+        _logger.LogInformation("Adding mock rate response with status {Status}", status);
+        _mockConfigService.EnqueueResponse(rate, status);
+        return Ok();
+    }
+}
