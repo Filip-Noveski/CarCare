23c7db2b56061719750371212ea101904b33dc18
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2025-12-16 21:54:55 +0100

    Handle lifecycle in CarService [Processing]

--- src/CarCare.Processing/Services/CarService.cs
+++ src/CarCare.Processing/Services/CarService.cs
@@ -1,5 +1,6 @@
 ﻿using CarCare.Persistence.Interfaces;
 using CarCare.Persistence.Models;
+using CarCare.Processing.Enums;
 using CarCare.Processing.Interfaces.Service;
 using CarCare.Processing.Interfaces.Session;
 using CarCare.Processing.Models.Core;
@@ -9,17 +10,20 @@ namespace CarCare.Processing.Services;
 internal class CarService : ICarService
 {
     private readonly ICarRepository _carRepository;
+    private readonly ICarLifecycleRepository _carLifecycleRepository;
     private readonly IUserSession _session;
     private readonly IBitmapCreatorService _bitmapService;
 
     public CarService(
         ICarRepository carRepository,
         IUserSession session,
-        IBitmapCreatorService bitmapService)
+        IBitmapCreatorService bitmapService,
+        ICarLifecycleRepository carLifecycleRepository)
     {
         _carRepository = carRepository;
         _session = session;
         _bitmapService = bitmapService;
+        _carLifecycleRepository = carLifecycleRepository;
     }
 
     public async Task AddAsync(Car car)
@@ -30,6 +34,7 @@ internal class CarService : ICarService
         }
 
         await _carRepository.AddAsync(car.ToDao(_bitmapService));
+        await _carLifecycleRepository.AddAsync(car.LifecycleToDao());
     }
 
     public async Task DeleteAsync(Guid id)
@@ -40,26 +45,54 @@ internal class CarService : ICarService
             return;
         }
 
+        await _carLifecycleRepository.DeleteAsync(id);
         await _carRepository.DeleteAsync(id);
     }
 
     public async Task<IEnumerable<Car>> GetAllByUserIdAsync(Guid userId, int page = 1, int count = 10)
     {
         IEnumerable<CarDao> result = await _carRepository.GetAllByUserAsync(userId, page, count);
-        return result.Select(
-            x => new Car(
+        IEnumerable<Task<Car>> tasks = result.Select(async x =>
+        {
+            CarLifecycleDao lifecycleDao = await _carLifecycleRepository.GetAsync(x.Id);
+            CarLifecycle lifecycle = new(
+                lifecycleDao.PurchaseDate,
+                lifecycleDao.PurchasePrice,
+                Enum.Parse<Currency>(lifecycleDao.PurchaseCurrency, ignoreCase: true),
+                lifecycleDao.SellDate,
+                lifecycleDao.SellPrice,
+                lifecycleDao.SellCurrency is null 
+                    ? null 
+                    : Enum.Parse<Currency>(lifecycleDao.SellCurrency, ignoreCase: true));
+
+            return new Car(
                 x.Id,
                 x.UserId,
                 x.Manufacturer,
                 x.Model,
                 x.Specification,
                 x.ModelYear,
-                x.Image is null ? null : _bitmapService.ConvertToBitmap(x.Image)));
+                x.Image is null ? null : _bitmapService.ConvertToBitmap(x.Image),
+                lifecycle);
+        });
+
+        return await Task.WhenAll(tasks);
     }
 
     public async Task<Car> GetAsync(Guid id)
     {
         CarDao dao = await _carRepository.GetAsync(id);
+        CarLifecycleDao lifecycleDao = await _carLifecycleRepository.GetAsync(id);
+        CarLifecycle lifecycle = new(
+            lifecycleDao.PurchaseDate,
+            lifecycleDao.PurchasePrice,
+            Enum.Parse<Currency>(lifecycleDao.PurchaseCurrency, ignoreCase: true),
+            lifecycleDao.SellDate,
+            lifecycleDao.SellPrice,
+            lifecycleDao.SellCurrency is null
+                ? null
+                : Enum.Parse<Currency>(lifecycleDao.SellCurrency, ignoreCase: true));
+
         return new(
             dao.Id,
             dao.UserId,
@@ -67,7 +100,8 @@ internal class CarService : ICarService
             dao.Model,
             dao.Specification,
             dao.ModelYear,
-            dao.Image is null ? null : _bitmapService.ConvertToBitmap(dao.Image));
+            dao.Image is null ? null : _bitmapService.ConvertToBitmap(dao.Image),
+            lifecycle);
     }
 
     public async Task UpdateAsync(Car car)
@@ -78,5 +112,6 @@ internal class CarService : ICarService
         }
 
         await _carRepository.UpdateAsync(car.ToDao(_bitmapService));
+        await _carLifecycleRepository.UpdateAsync(car.LifecycleToDao());
     }
 }
